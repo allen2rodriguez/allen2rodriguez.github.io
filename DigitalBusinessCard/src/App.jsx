d8e3a757863f034c4747c69f33d7a37243e77f22
Allen Rodriguez <aerodr26@asu.edu>
2025-06-26 16:44:24 -0700

    Adding a digital business card

---
+++ DigitalBusinessCard/src/App.jsx
@@ -0,0 +1,13 @@
+import Info from './components/info.jsx';
+import About from './components/about.jsx';
+import Footer from './components/footer.jsx';
+
+export default function App() {
+    return (
+        <>
+            <Info />
+            <About />
+            <Footer />
+        </>
+    )
+}
---
+++ DigitalBusinessCard/src/components/about.jsx
@@ -0,0 +1,17 @@
+export default function About() {
+    return (
+        <div className="about">
+            <h3 className="about-title">About</h3>
+            <p className="about-text">
+                Hi, I'm a Computer Science and Business Management student at ASU with experience in data analysis, programming, and product development. 
+                I’m passionate about innovation and learning.
+                I bridge the gap between business and technology, and I love building intelligent user-friendly apps like my wearable tech concept TechStyle.
+            </p>
+            <h3 className="about-title">Interests</h3>
+            <p className="about-text"> 
+                In my free time I enjoy playing the piano, wrenching on cars, coding, building computers, dancing tango and spending time with family and friends.
+            </p>
+                
+        </div>
+    )
+}

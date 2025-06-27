763da1fdc9a44ae5d0d0dd76e47c965d630130f0
Allen Rodriguez <aerodr26@asu.edu>
2025-06-26 19:10:22 -0700

    Correcting card

--- DigitalBusinessCard/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import {createRoot} from 'react-dom/client'
-import App from './app.jsx'
-
-const rootElement = createRoot(document.getElementById('root'));
-rootElement.render(<App />);
--- DigitalBusinessCard/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// vite.config.js
-export default {
-  base: '/', // if your repo is github.com/username/DigitalBusinessCard
-};

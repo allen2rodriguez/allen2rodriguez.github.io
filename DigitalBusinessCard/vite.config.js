4c7f193d2850de26e514da63544619e3f6405509
Allen Rodriguez <aerodr26@asu.edu>
2025-06-26 18:52:04 -0700

    Fix 2

--- DigitalBusinessCard/vite.config.js
+++ DigitalBusinessCard/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  base: '/', // if your repo is github.com/username/DigitalBusinessCard
+};
